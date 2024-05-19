76537ba493d4b3bfedb6ea20cfe7f6d271adc558
rahuvash <rahulvashisth052@gmail.com>
2024-05-19 22:18:57 +0530

    hashed pass

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,12 +1,20 @@
 // controllers/authController.js
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 
 exports.register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        const user = new User({ name, email, password });
-        // console.log('User information:', { name, email, password });
-        console.log(`User registere : ${name}`)
+
+        // Log the user information
+        console.log('User information:', { name, email, password });
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create a new user with the hashed password
+        const user = new User({ name, email, password: hashedPassword });
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
